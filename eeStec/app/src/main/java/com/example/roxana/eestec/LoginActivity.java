2ddaf277385f97e34e8f13643c5bc8f7a9068522
Roxana Macarie <Roxana Macarie>
2017-05-10 21:37:08 +0300

    changed design

--- eeStec/app/src/main/java/com/example/roxana/eestec/LoginActivity.java
+++ eeStec/app/src/main/java/com/example/roxana/eestec/LoginActivity.java
@@ -205,7 +205,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.contains("@eestec");
     }
 
     private boolean isPasswordValid(String password) {
